c4a7c76470de9f65daaa61051e79531c249803f4
Lester Carpenter <lsysworks@gmail.com>
2022-02-08 11:00:01 -0500

    Working Rock Paper Scissors App, v.01; cleanup
    TO DO:
      - js/rps.js
        * Commit to separate repo for final version
        * Simplify computerPlay() function — remove if / else if stmts
    
    - js/rps.js
      * Rock Paper Scissors App, version .01 (console only)
      * General clean up; consolidate comments

--- js/rps.js
+++ js/rps.js
@@ -1,16 +1,10 @@
-
-// function computerPlay(compNum) {
-//   if (compNum > 1 && compNum < 33) {
-//     console.log('Rock is chosen');
-//   } else if (compNum > 33 && compNum < 66) {
-//     console.log('Paper is chosen');
-//   } else if (compNum > 66 && compNum < 100) {
-//     console.log('Scissors is chosen');
-//   }
-// }
-// computerPlay();
-// Begin with a function called computerPlay which will randomly return either 'Rock', 'Paper', or 'Scissors'.
-https://medium.com/swlh/random-functions-a4f36b1dfd8f
+/*
+ * Rock Paper Scissors App
+ * https://www.theodinproject.com/paths/foundations/courses/foundations/lessons/rock-paper-scissors#assignment
+ */
+// Optimize computerPlay, or at least try
+// what is the relationship btwn Math.random and the number by which it's multiplied?
+// how can this be altered to more randomize it, or is that even possible, advisable? 
 function computerPlay(randoNum) {
   randoNum = parseInt(Math.random() * (100 - 1) + 7);
   if (randoNum > 100) {
@@ -25,185 +19,87 @@ function computerPlay(randoNum) {
   } else if (randoNum >= 34 && randoNum <= 66) {
     // console.log('Scissor: ', randoNum);
     return randoNum = 'scissors';
-  } else {
+  } else if (randoNum >= 67 && randoNum <= 100) {
     // console.log('Paper: ', randoNum);
     return randoNum = 'paper';
   }
 }
+
+ 
 // computerPlay();
 
-// Write a function that plays one round of RPS.
-// It should take two parameters, playerSelection and computerSelection.
-// It should return a string stating which player won the round, "Sorry, Rock beats Scissors."
-// Ensure that humanSelection is case insensitive.
 // playGame();
 function playGame() {
   let computerScore = 0;
   let humanScore = 0;
-  for (let i = 1; ; i++) {
+  // you want statement to return false as soon as one of your conditions turns false
+  // you want your loop to run as long as both conditions are true '@riri's babybump'
+  // && returns TRUE if BOTH operands are TRUTHY and FALSE otherwise.
+  for (let i = 1; humanScore < 5 && computerScore < 5; i++) { 
     const computerSelection = computerPlay();
-    // parseInt(humanScore);
+
     // console.log('this is computerSelection: ', computerSelection);
-    const humanSelection = prompt('Make your selection: Rock, Paper, or Scissors');
+    let humanSelection = prompt('Make a selection: Rock, Paper, or Scissors');
+    // playRound(humanSelection, computerSelection); 
     
-    // playRound(humanSelection, computerSelection);
     function playRound(humanSelection, computerSelection) {
-      // convert to switch stmt
-      if (humanSelection === 'rock' && computerSelection === 'rock') {
-        console.log('It\'s a tie');
-      } else if (humanSelection === 'rock' && computerSelection === 'scissors') {
-        console.log(`${humanSelection} CRUSHES ${computerSelection}`);
-        humanScore++;
-        // console.log(`This is humanScore ${humanScore} and this is computerScore ${computerScore}`);
-      } else if (humanSelection === 'rock' && computerSelection === 'paper') {
-        console.log(`${computerSelection} SMOTHERS ${humanSelection}`);
-        computerScore++;
-        // console.log(`This is humanScore ${humanScore} and this is computerScore ${computerScore}`);
-      } else if (humanSelection === 'paper' && computerSelection === 'rock') {
-        console.log(`${humanSelection} SMOTHERS ${computerSelection}`);
-        humanScore++;
-        // console.log(`This is humanScore ${humanScore} and this is computerScore ${computerScore}`);
-      } else if (humanSelection === 'paper' && computerSelection === 'scissors') {
-        console.log(`${computerSelection} SLICE ${humanSelection}`);
-        computerScore++;
-        // console.log(`This is humanScore ${humanScore} and this is computerScore ${computerScore}`);
-      } else if (humanSelection === 'paper' && computerSelection === 'paper') {
-        console.log(`${computerSelection} It\'s a tie ${humanSelection}`);
-      } else if (humanSelection === 'scissors' && computerSelection === 'rock') {
-        computerScore++;
-        console.log(`${computerSelection} CRUSHES ${humanSelection}`);
-      } else if (humanSelection === 'scissors' && computerSelection === 'scissors') {
-        console.log(`${computerSelection} It\'s a tie ${humanSelection}`);
-      } else if (humanSelection === 'scissors' && computerSelection === 'paper') {
-        humanScore++;
-        console.log(`${humanSelection} SLICE ${computerSelection}`);
-        // console.log(`This is humanScore ${humanScore} and this is computerScore ${computerScore}`);
+      humanSelection = humanSelection.toLowerCase();
+      // convert to an array (or is object better?) and loop through results
+      switch (true) {
+        case humanSelection === 'rock' && computerSelection === 'rock':
+          console.log('It\'s a tie.');
+          break;
+        case humanSelection === 'rock' && computerSelection === 'paper':
+          console.log(`${computerSelection} SMOTHERS ${humanSelection}`);
+          computerScore++;
+          break;
+        case humanSelection === 'rock' && computerSelection === 'scissors':
+          console.log(`${humanSelection} CRUSHES ${computerSelection}`);
+          humanScore++;
+          break;
+        
+        case humanSelection === 'paper' && computerSelection === 'paper':
+          console.log('It\'s a tie.');
+          break;
+        case humanSelection === 'paper' && computerSelection === 'scissors':
+          console.log(`${computerSelection} SLICES ${humanSelection}`);
+          computerScore++;
+          break;
+        case humanSelection === 'paper' && computerSelection === 'rock':
+          console.log(`${humanSelection} SMOTHERS ${computerSelection}`);
+          humanScore++;
+          break;
+        
+        case humanSelection === 'scissors' && computerSelection === 'scissors':
+          console.log('It\'s a tie.');
+          break;
+        case humanSelection === 'scissors' && computerSelection === 'rock':
+          console.log(`${computerSelection} CRUSHES ${humanSelection}`);
+          computerScore++;
+          break;
+        case humanSelection === 'scissors' && computerSelection === 'paper':
+          console.log(`${humanSelection} SLICES ${computerSelection}`);
+          humanScore++;
+          break;
       }
+
       console.log(`humanScore: ${humanScore}`);
       console.log(`computerScore: ${computerScore}`);
 
       // return `${humanSelection} and ${computerSelection}`;
-    }
+    } // playRound
     playRound(humanSelection, computerSelection);
+    console.log(`This is humanSelection ${humanSelection}`);
     console.log(`This is computerSelection ${computerSelection}`);
+  } // for loop
+} // playGame
 
-    // if (humanScore >= 5) {
-    //   console.log('Congratulations! You beat Hal!');
-    //   break;
-    // } else if (computerScore >= 5) {
-    //   console.log('Congratulations! You beat the meatbag human!');
-    //   break;
-    // } 
-  }
+// get random integer
+function getRandoInt(max, min) {
+  // return randoInt = .4053117146711769 * (10 - 1) + 1;
+  let someInt = Math.random(); // 3353563462989505 Math.random()
+  const randInt = someInt * (10 - 1) + 1;
+  // return randoInt = Math.random() * (max - min) + 1;
 }
-// function playRound(humanSelection, computerSelection) {
-//   console.log('this is humanSelection: ', humanSelection);
-// }
-// function playRound(humanSelection, computerSelection) {
-//   // console.log('this is humanSelection: ', humanSelection);
-//   // convert to switch stmt
-//   if (humanSelection === 'rock' && computerSelection === 'rock') {
-//     console.log('It\'s a tie');
-//   } else if (humanSelection === 'rock' && computerSelection === 'scissors') {
-//     console.log(`${humanSelection} CRUSHES ${computerSelection}`);
-//     humanScore++;
-//     // console.log(`This is humanScore ${humanScore} and this is computerScore ${computerScore}`);
-//   } else if (humanSelection === 'rock' && computerSelection === 'paper') {
-//     console.log(`${computerSelection} SMOTHERS ${humanSelection}`);
-//     computerScore++;
-//     // console.log(`This is humanScore ${humanScore} and this is computerScore ${computerScore}`);
-//   } else if (humanSelection === 'paper' && computerSelection === 'rock') {
-//     console.log(`${humanSelection} SMOTHERS ${computerSelection}`);
-//     humanScore++;
-//     // console.log(`This is humanScore ${humanScore} and this is computerScore ${computerScore}`);
-//   } else if (humanSelection === 'paper' && computerSelection === 'scissors') {
-//     console.log(`${computerSelection} SLICE ${humanSelection}`);
-//     computerScore++;
-//     // console.log(`This is humanScore ${humanScore} and this is computerScore ${computerScore}`);
-//   } else if (humanSelection === 'paper' && computerSelection === 'paper') {
-//     console.log(`${computerSelection} It\'s a tie ${humanSelection}`);
-//   } else if (humanSelection === 'scissors' && computerSelection === 'rock') {
-//     computerScore++;
-//     // console.log(`${computerSelection} CRUSHES ${humanSelection}`);
-//   } else if (humanSelection === 'scissors' && computerSelection === 'scissors') {
-//       console.log(`${computerSelection} It\'s a tie ${humanSelection}`);
-//   } else if (humanSelection === 'scissors' && computerSelection === 'paper') {
-//     humanScore++;
-//     // console.log(`${humanSelection} SLICE ${computerSelection}`);
-//     // console.log(`This is humanScore ${humanScore} and this is computerScore ${computerScore}`);
-//   }
-//   console.log(`humanScore: ${humanScore}`);
-//   console.log(`computerScore: ${computerScore}`);
-//   computerScore++;
-
-//     return `${humanSelection} and ${computerSelection}`;
-// }
-
-  // playRound();
-  // // const computerSelection = computerPlay();
-  // // humanSelection will become a prompt but for now, enter manually.
-  // const humanSelection = 'rock'; // rock scissors paper
-  // console.log('This is playRound:', playRound(humanSelection.toLowerCase(), computerSelection));
-
-// 2nd attempt at RPS
-// function game2() {
-//   // let humanSelection2 = prompt('Rock, Paper, or Scissors, mate?');
-//   let humanScore = 0;
-//   let computerScore = 0;
-
-//   for (let i = 1; i < 5; i++) {
-//     playRound(); 
-//     console.log(i);
-//   }
-// }
-// game2();
-// compares each param and returns a winner
-// function playRound(humanSelection2, computerSelection2) {
-//   humanSelection2 = 'rock';
-//   computerSelection2 = 'scissors';
-//   if (humanSelection2 === 'rock') {
-//     console.log('Need to increment humanScore');
-//   } else if (computerSelection2 === 'scissors') {
-//     console.log('Need to increment computerScore');
-//   }
-// }
-
-
-// if (humanSelection2 === 'rock') {
-//   ++humanScore;
-//   console.log(humanScore);
-
-// function gameplay(playerSelection) {
-//   //First, we let the computer pick a random guess
-//   let computeranswer = computerPlay();
-//   let playeranswer = playerSelection.toLowerCase();
-//   let Tie = "Tie";
-//   let ComputerWins = "Computer Wins";
-//   let YouWin = "You Win";
-//   let Fail = "Fail";
-//   //Logic for if you picked rock
-//   if (playeranswer == "rock" && computeranswer == "Rock") {
-//     return Tie;
-//   } else if (playeranswer == "rock" && computeranswer == "Paper") {
-//     return ComputerWins;
-//   } else if (playeranswer == "rock" && computeranswer == "Scissors") {
-//     return YouWin;
-//   } else if (playeranswer == "paper" && computeranswer == "Paper") {
-//     return Tie;
-//   } else if (playeranswer == "paper" && computeranswer == "Scissors") {
-//     return ComputerWins;
-//   } else if (playeranswer == "paper" && computeranswer == "Rock") {
-//     return YouWin;
-//   } else if (playeranswer == "rock" && computeranswer == "Rock") {
-//     return Tie;
-//   } else if (playeranswer == "rock" && computeranswer == "Paper") {
-//     return ComputerWins;
-//   } else if (playeranswer == "rock" && computeranswer == "Scissors") {
-//     return ComputerWins;
-//   }
-//   else {
-//     return Fail;
-//   }
-// } gameplay('rock');
-// console.log(gameplay('rock'));
-// https://codepen.io/thomasbrown98/pen/OJOboVj?editors=0010 
+getRandoInt();
+// .4053117146711769 * (10 - 1) + 1 
