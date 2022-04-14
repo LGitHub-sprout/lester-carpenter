f2c4a5536a4a9ab60c7b3d1e25950a3d912357ee
LesterC <lsysworks@gmail.com>
2022-04-14 13:51:42 -0400

    Add Final TOP TDD test 05_sumAll
    - js/odin-tests.js
      * Passed All

--- js/odin-tests.js
+++ js/odin-tests.js
@@ -4,79 +4,85 @@
   @link https://github.com/TheOdinProject/javascript-exercises
 */
 
-/***********************************************************
-  # Exercise 05 - sumAll
+/************************* # Exercise 05 - sumAll()  *********************
 
   Implement a function that takes 2 integers and returns the sum of every number between(and including) them:
-
+  
   ```javascript
   sumAll(1, 4) // returns the sum of 1 + 2 + 3 + 4 which is 10
   ```
-  Pseudocode:
-  Beginning w minNum, loop to maxNum
-  Add ea iteration result to the next, sequentially
-  so that sumTotal is minNum +1 until maxNum is reached.
-  Return sumTotal
-
-  possible solutions to 
-  sumAll(123, 1)); // works with larger number first .toEqual(7626)
-    the problem is that i'm setting i to firstNum and decrementing thru loop
-      to add each iteration to sumTotal.
-      if the args are reversed then this logic fails.
-    could i save one var for the other if secondNum > firstNum?
-    Could i set i to always be biggest num regardless of whether that's firstNum or second Num?
-
-  @param  firstNum     integer
-  @param  secondNum    integer
-  @param  sumTotal     integer
+  Notes:
   https://stackoverflow.com/questions/12691142/javascript-for-loop-until-multiple-conditions
+  Math.max and Math.min but nums need to be in an array
+
+  Pseudocode: (not how the algorithm went, lol but it got done.)
+    Beginning w firstNum, loop to secondNum
+    Add ea iteration result counter to the next, sequentially,
+    so that sumTotal is minNum +1 until maxNum is reached.
+    Return sumTotal
+
+  problem: 
+  sumAll(123, 1)); // doesn't with larger number first .toEqual(7626)
+    the problem is that i'm setting i to firstNum and incrementing thru loop
+      to add each iteration to sumTotal.
+      if the args are reversed then this logic fails due to condition = false.
+    could i save one var for the other if secondNum > firstNum? no, hoisting.
+      this is the answer that I used to solve it and used variables in if/else stmt.
+    Could I set i to always be biggest num regardless of whether that's firstNum or second Num?
+      Not sure how. 
+    Larger num first works if conditional is >=; and i-- decremented
+      are such things are multiple conditional loops
+      or can i set if stmts first, then loop? again, no--hoisting.
+        On the right track here... and ultimately used variables this way.
+    
+    what appears to be the point of this one?
+      there has to be clues in the link
+      that i can use multiple conditions in a loop?
+      that i can use if conditional to run multiple different loops?
+
+    Pseudocode: fixes "large number first"
+    if firstNum is greater than secondNum 
+      set maxNum to firstNum // save firstNum to diff var for processing
+      set minNum to secondNum
+    else if firstNum is less-than  
+    while the counter is less than maxNum
+      increment the counter;
+      if (firstNum < secondNum)
+
+  @param  firstNum  integer
+  @param  secondNum integer
+
+  @var    sumTotal  integer
+  @var    minNum    integer
+  @var    maxNum    integer
+
+  @return sumTotal
 */ 
 const sumAll = function (firstNum, secondNum) {
-
   let sumTotal = 0;
-  // if i = 0; then how do i know whether to decrement or increment?
-  
-  // if (firstNum > 1) {
-  //   i = secondNum; 
-  //   // console.log('firstNum: ', firstNum)
-  //   // console.log('secondNum: ', secondNum)
-  // }
-  for (let i = firstNum; i <= secondNum; i++) {
-    if (firstNum < 0 || secondNum < 0) return 'ERROR';
-    if (typeof firstNum === 'string' || typeof secondNum === 'string') return 'ERROR';
 
+  if (firstNum > 1) {
+    minNum = secondNum; 
+    maxNum = firstNum;
+  } else if (firstNum < secondNum) {
+    minNum = firstNum;
+    maxNum = secondNum; 
+  }
+  // if (firstNum !== 'number' || secondNum !== 'number') return 'ERROR';
+
+  if (typeof minNum !== 'number' || typeof maxNum !== 'number') return 'general ERROR';
+  
+  for (let i = minNum; i <= maxNum; i++) { // i always needs to be lowest num
+    if (firstNum < 0 || secondNum < 0) return 'negative number ERROR';
+    
     sumTotal += i; // adds indexes to sumTotal as it loops 
   }
-  console.log(sumTotal) // return sumTotal for testing purposes
+
+  console.log(typeof sumTotal, sumTotal) //  return sumTotal;
 };
-console.log(sumAll(1, 4000)); // works with larger number first .toEqual(7626) if 
-// (let i = firstNum; i >= secondNum; --i)
+console.log(sumAll(-10, 4));
 
-// console.log(sumAll(123, 1)); // works with larger number first .toEqual(7626) if 
-// console.log(sumAll(10, [90, 1])); // works with larger number first .toEqual(7626)
-// console.log(sumAll(10, "90")); // works with larger number first .toEqual(7626)
-// console.log(sumAll(-10, 4)); // works with larger number first .toEqual(7626)
-
-/* PASSED */
-// console.log(sumAll(1, 4000)); // works with large numbers .toEqual(8002000)
-// console.log(sumAll(1, 4)); // returns the sum of 1 + 2 + 3 + 4 which is 10
-
-/*
-  Working on 3rd test 'larger number first'
-  // if (firstNum > 1) {
-  //   i = secondNum; 
-  //   // console.log('firstNum: ', firstNum)
-  //   // console.log('secondNum: ', secondNum)
-  // }
-  for (let i = firstNum; i >= secondNum; i--) {
-    if (firstNum < 0 || secondNum < 0) return 'ERROR';
-    if (typeof firstNum === 'string' || typeof secondNum === 'string') return 'ERROR';
-*/
-
-
-/***********************************************************
-  # Exercise 04 - removeFromArray
-
+/************************* # Exercise 04 - removeFromArray()  *********************
   Implement a function that takes an array and some other arguments then removes the other arguments from that array:
 
   ```javascript
@@ -111,8 +117,7 @@ const removeFromArray = function (arr, ...theArgs) {
 };
 removeFromArray([1, 2, 3, 4], 4, 3);
 
-/***********************************************************
-  # Exercise 03 - Reverse a String
+/************************* # Exercise 03 - reverseString() *********************
 
   Pretty simple, write a function called `reverseString` that returns its input, reversed!
 
@@ -143,8 +148,7 @@ const reverseString = function (string) {
   return string.split('').reverse().join('');
 };
 
-/***********************************************************
-  Exercise 02 - repeatString
+/************************* # Exercise 02 - repeatString() *********************
 
   Write a function that simply repeats the string a given number of times:
 
