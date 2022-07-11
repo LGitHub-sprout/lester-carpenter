c5fbdf904c39e05729a33c8692e9b330a9013d54
LesterC <lsysworks@gmail.com>
2022-07-11 13:32:24 -0400

    EaS - CSS Bugfix: Random Grid Gaps; Change to %s
    - js/etch-a-sketch.js
      * Change height/width calc to percentages.
      * Clean code.
    
    - odin-project/etch-a-sketch.html
      * Add 'Problems So Far' section.
      * Add 'etch-a-sketch' class on HTML for 'line-height' property.
    
    - styles/odin-project-styles/odin.css
      * Bugfix: remove random gaps in grid area at larger sizes.
      * Rework flexbox properties for bugfix:
        - Add 'align-content'
        - Change .sketch-container width

--- js/etch-a-sketch.js
+++ js/etch-a-sketch.js
@@ -1,103 +1,43 @@
-// var form = document.getElementById('addForm');
-// var itemList = document.getElementById('items');
-// var filter = document.getElementById('filter');
 
-// // Form submit event
-// form.addEventListener('submit', addItem);
-// // Delete event
-// itemList.addEventListener('click', removeItem);
-// // Filter event
-// // filter.addEventListener('keyup', filterItems);
-
-// // Add item
-// function addItem(e){
-//   e.preventDefault();
-
-//   // Get input value
-//   var newItem = document.getElementById('item').value;
-
-//   // Create new li element
-//   var li = document.createElement('li');
-//   // Add class
-//   li.className = 'list-group-item';
-//   // Add text node with input value
-//   li.appendChild(document.createTextNode(newItem));
-
-//   // Create del button element
-//   var deleteBtn = document.createElement('button');
-
-//   // Add classes to del button
-//   deleteBtn.className = 'btn btn-danger btn-sm float-right delete';
-
-//   // Append text node
-//   deleteBtn.appendChild(document.createTextNode('X'));
-
-//   // Append button to li
-//   li.appendChild(deleteBtn);
-
-//   // Append li to list
-//   itemList.appendChild(li);
-// }
-
-// Remove item
-// function removeItem(e){
-//   if(e.target.classList.contains('delete')){
-//     if(confirm('Are You Sure?')){
-//       var li = e.target.parentElement;
-//       itemList.removeChild(li);
-//     }
-//   }
-// }
-
-// Filter Items
-// function filterItems(e){
-//   // convert text to lowercase
-//   var text = e.target.value.toLowerCase();
-//   // Get lis
-//   var items = itemList.getElementsByTagName('li');
-//   // Convert to an array
-//   Array.from(items).forEach(function(item){
-//     var itemName = item.firstChild.textContent;
-//     if(itemName.toLowerCase().indexOf(text) != -1){
-//       item.style.display = 'block';
-//     } else {
-//       item.style.display = 'none';
-//     }
-//   });
-// }
 const divContainer = document.querySelector('.sketch-container');
-
 // Create the number of grid-items based on user input
 // user input = 13
 // grid-container = 400px
 // 400 / 13 = 30.77px wide
 // 13 * 13 = 169 items
 
-let squaresPerSide = prompt('How many squares down should the grid be?', 60);
-
+// let squaresPerSide = prompt('How many squares down should the grid be?', 60);
 function calcGrid(squaresPerSide) {
 
+  // const gridHeight = Math.floor(400 / squaresPerSide);
+  // const gridWidth = Math.floor(400 / squaresPerSide);
+  
+  // Set a default
   const gridArea = Math.pow(squaresPerSide, 2);
-  const gridHeight = Math.floor(400 / squaresPerSide);
-  const gridWidth = Math.floor(400 / squaresPerSide);
+  // const gridHeight = Number(400 / squaresPerSide).toFixed(15);
+  // const gridWidth = Number(400 / squaresPerSide).toFixed(15);
+  const gridHeight = Number(100 / squaresPerSide).toFixed(15);
+  const gridWidth = Number(100 / squaresPerSide).toFixed(15);
 
   for (let i = 0; i < gridArea; i++) {
     const gridItem = document.createElement('div');
     gridItem.classList = 'grid-square';
-    gridItem.style.cssText = `height:${gridHeight}px; width:${gridWidth}px`;
+    gridItem.style.cssText = `height:${gridHeight}%;flex: 0 0 ${gridWidth}%`;
+    // gridItem.style.cssText = `height:${gridHeight}px; width:${gridWidth}px`;
     // gridItem.style.cssText = 'width: 20px;height: 20px;border-right: solid .25px black;border-bottom: solid .25px black;';
     divContainer.appendChild(gridItem);
+    // console.log('Height: ', gridHeight)
+    // console.log('Width: ', gridWidth)
   }
-  console.log(gridArea)
-  console.log(typeof gridHeight, gridWidth)
+  console.log('gridArea: ', gridArea)
 }
-calcGrid(squaresPerSide)
+calcGrid(100)
 // area of a square is: square = 20(side 1) * 20(side 2)
 // So I'd use division to calc the size of each square?
 // Number of grid-items / square area?
 // If the side is always 20 (20 * 20 = 400)  
-// const randNum = Math.floor(Math.random() * 60);
-// console.log(randNum)
+const randNum = Math.floor(Math.random() * 60);
+console.log('randNum: ', randNum)
 
 // user input = 33
 // 400 / 33 = 12px wide
