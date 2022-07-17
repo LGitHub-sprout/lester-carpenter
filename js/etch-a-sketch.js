4db92c3ed2702cfabc75ea8ac22e7f477bd7c8d4
LesterC <lsysworks@gmail.com>
2022-07-17 12:51:30 -0400

    EaS: Add Grid Wrap & JS Conditionals; Clean Code
    - js/etch-a-sketch.js
      * Add sketchContainer var: remove firstElementChild if new grid.
      * Add grid size conditionals for bad user input, null, etc.
      * Add 'mouseover' event to gridSquares: classList.add and toggle.
    
    - odin-project/etch-a-sketch.html
      * Add sketch-container-wrapper for new userGrid.
      * Add 'Problems' and 'Hover Effects' sections.
      * Clean code.
    
     - styles/odin-project-styles/odin.css
      * Bug fix: remove article max-height: 600px
      * Add gridSquares background class properties.

--- js/etch-a-sketch.js
+++ js/etch-a-sketch.js
@@ -1,81 +1,104 @@
 
-const divContainer = document.querySelector('.sketch-container');
-const setGridSize = document.querySelector('.set-grid-size');
+const sketchContainerWrap = document.querySelector('.sketch-container-wrapper');
+const setGridSizeBtn = document.querySelector('.set-grid-size');  
+// const rainbowBtn = document.querySelector('.bg-rainbow');
 let userChoice;
-let squaresPerSide;
 
-setGridSize.addEventListener('click', function () {
+
+// const rainbowGridSquares = sketchContainerWrap.children;
+// console.log(Array.from(rainbowGridSquares));
+// console.log(rainbowGridSquares)
+// console.log(rainbowGridSquares.length)
+// console.log(rainbowGridSquares[0])
+// const rainbowGridSquares = sketchContainerWrap.childNodes;
+// for (let i = 0; i < rainbowGridSquares.length; i++) {
+//   console.log(rainbowGridSquares[i].nextSibling) // null 
+//   console.log(rainbowGridSquares[i].childNodes) // empty nodeList[] ?? 
+//   console.log('lastChild', rainbowGridSquares[i].lastChild) // null
+// }
+// console.log('rainbowGridSquares: ', rainbowGridSquares)
+// console.log('rainbowGridSquares length: ', rainbowGridSquares.length)
+// console.log('nextSibling ', rainbowGridSquares[0].nextSibling) // null
+// console.log('nextSibling ', rainbowGridSquares.nextSibling) // null
+// console.log('lastChild ', rainbowGridSquares[0].lastChild) // null
+// console.log('childNodes ', rainbowGridSquares[0].childNodes) // null
+
+// for (const entry of rainbowGridSquares.entries()) {
+//   console.log('sketch container wrap\'s childNodes', entry)
+// }
+
+
+setGridSizeBtn.addEventListener('click', function (e) {
   userChoice = prompt('How many squares across should the grid be?');
+  if ((userChoice > 100) || (userChoice === '') || Math.sign(userChoice) === -1) {
+    alert('The maximum number of allowed squares is 100 and the minimum is 1.');
+    return;
+  }
+  if (userChoice === null) return;
+  if (Math.sign(userChoice) === 0) {
+    alert('Zero, really? SMH');
+    return;
+  }
   calcGrid();
 });
-// setGridSize.addEventListener('click', calcGrid);
 
-// Create the number of grid-items based on user input
-// user input = 13
-// grid-container = 400px
-// 400 / 13 = 30.77px wide
-// 13 * 13 = 169 items
-
-// let squaresPerSide = prompt('How many squares down should the grid be?', 60);
-// squaresPerSide = Math.floor(Math.random() * 100);
-// squaresPerSide = 16;
 function calcGrid(squaresPerSide) {
-
-  // userChoice = prompt('How many squares down should the grid be?');
   if (userChoice) {
     squaresPerSide = userChoice;
-    console.log('userChoice', userChoice)
+    sketchContainerWrap.firstElementChild.remove();
   } else squaresPerSide = 16;
 
-  // const gridHeight = Math.floor(400 / squaresPerSide);
-  // const gridWidth = Math.floor(400 / squaresPerSide);
-  
   const gridArea = Math.pow(squaresPerSide, 2);
-  // const gridHeight = Number(500 / squaresPerSide).toFixed(15);
-  // const gridWidth = Number(500 / squaresPerSide).toFixed(15);
-  const gridHeight = Number(100 / squaresPerSide).toFixed(15);
-  const gridWidth = Number(100 / squaresPerSide).toFixed(15);
+  const gridHeight = Number(100 / squaresPerSide).toFixed(5);
+  const gridWidth = Number(100 / squaresPerSide).toFixed(5);
+  
+  const sketchContainer = document.createElement('div');
+  sketchContainer.classList = 'sketch-container';
+  sketchContainerWrap.appendChild(sketchContainer);
 
+  
   for (let i = 0; i < gridArea; i++) {
-    const gridItem = document.createElement('div');
-    gridItem.classList = 'grid-square';
-    gridItem.style.cssText = `height:${gridHeight}%; flex: 0 0 ${gridWidth}%`;
-    // gridItem.style.cssText = `height:${gridHeight}px; width:${gridWidth}px`;
-    // gridItem.style.cssText = 'width: 20px;height: 20px;border-right: solid .25px black;border-bottom: solid .25px black;';
-    divContainer.appendChild(gridItem);
+    gridSquare = document.createElement('div');
+    gridSquare.classList = 'grid-square';
+
+    /*
+      I only have access to all the gridSquares inside the for loop.
+      Therefore, in the loop, set all the different classes needed to change 
+      the grid colors on mouseover event. 
+      Then somehow toggle, add, or remove them when buttons are clicked?
+      Is it a simple matter of using conditionals?
+
+      Does it go inside the 
+      
+      I also think I have a scoping problem bc I can't access ALL gridSquares 
+      anywhere other than inside the LOOP.
+
+      What matters is that I have access to ALL the gridSquares 
+      which means working inside the for loop.
+      
+      If gridSquare doesn't have 'rainbow' class attribute, 
+      add it (and possibly remove or otherwise toggle OFF all other classes);
+    */
+    // gridSquare.setAttribute('name', 'red');
+
+    gridSquare.style.cssText = `height:${gridHeight}%; flex: 0 0 ${gridWidth}%`;
+
+    sketchContainer.appendChild(gridSquare);
     // console.log('Height: ', gridHeight)
     // console.log('Width: ', gridWidth)
+
+    gridSquare.addEventListener('mouseover', function (e) {
+      e.target.classList.add('blackBg');
+      e.target.classList.toggle('red');
+    }); 
+
+    // rainbowBtn.addEventListener('click', function (e) {
+    //   // I need reference to all the gridSquares so I can change their color. 
+    //   gridSquare.toggleAttribute('red')
+    //   console.log(gridSquare)
+    // });
   }
-  console.log('gridArea: ', gridArea)
-  console.log('squaresPerSide', squaresPerSide)
+  // console.log('squaresPerSide', squaresPerSide)
+  // console.log('gridArea: ', gridArea)
 }
 calcGrid()
-// area of a square is: square = 20(side 1) * 20(side 2)
-// Number of grid-items / square area
-// If the side is always 20 (20 * 20 = 400)  
-// const randNum = Math.floor(Math.random() * 60);
-// console.log('randNum: ', randNum)
-
-// user input = 33
-// 400 / 33 = 12px wide
-// 33 * 33 = 1089 grid-items at 12.12px square
-
-// user input = 9
-// 400 / 9 = 44.44px
-// 9 * 9 = 81 grid-items
-
-// user input = 27
-// 400 / 27 = 14.814px
-// 27 * 27 = 729 grid-items
-
-// user input = 56
-// 400 / 56 = 7.14
-// 56 * 56 = 3136
-
-// user input = 12
-// 600 / 12 = 50
-// 12 * 12 = 144
-
-// user input = 17
-// 400 / 17 = 23.53
-// 17 * 17 = 289
