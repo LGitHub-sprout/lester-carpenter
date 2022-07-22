d8fa46a6ff6024134cb014883d1c845baf4283aa
LesterC <lsysworks@gmail.com>
2022-07-21 20:14:57 -0400

    EaS Bug Fix: Continuous Bg Change On 'mouseover'
    - js/etch-a-sketch.js
      * Bug Fix:
        - Make gridSquare bg color RGB on 'mouseover' C O N T I N U O U S.
        - Prev bg changed once, remained same w successive 'mouseover'.
    
    - odin-project/etch-a-sketch.html
      * Add 'Thinking About Hover Effects' section.
      * Reorganize sections chonologically: recent edits first.
    
     - styles/odin-project-styles/odin.css
      * Add set-grid-size bg

--- js/etch-a-sketch.js
+++ js/etch-a-sketch.js
@@ -1,32 +1,8 @@
 
 const sketchContainerWrap = document.querySelector('.sketch-container-wrapper');
 const setGridSizeBtn = document.querySelector('.set-grid-size');  
-// const rainbowBtn = document.querySelector('.bg-rainbow');
+const h1MainTitle = document.querySelector('.main-title');
 let userChoice;
-
-
-// const rainbowGridSquares = sketchContainerWrap.children;
-// console.log(Array.from(rainbowGridSquares));
-// console.log(rainbowGridSquares)
-// console.log(rainbowGridSquares.length)
-// console.log(rainbowGridSquares[0])
-// const rainbowGridSquares = sketchContainerWrap.childNodes;
-// for (let i = 0; i < rainbowGridSquares.length; i++) {
-//   console.log(rainbowGridSquares[i].nextSibling) // null 
-//   console.log(rainbowGridSquares[i].childNodes) // empty nodeList[] ?? 
-//   console.log('lastChild', rainbowGridSquares[i].lastChild) // null
-// }
-// console.log('rainbowGridSquares: ', rainbowGridSquares)
-// console.log('rainbowGridSquares length: ', rainbowGridSquares.length)
-// console.log('nextSibling ', rainbowGridSquares[0].nextSibling) // null
-// console.log('nextSibling ', rainbowGridSquares.nextSibling) // null
-// console.log('lastChild ', rainbowGridSquares[0].lastChild) // null
-// console.log('childNodes ', rainbowGridSquares[0].childNodes) // null
-
-// for (const entry of rainbowGridSquares.entries()) {
-//   console.log('sketch container wrap\'s childNodes', entry)
-// }
-
 
 setGridSizeBtn.addEventListener('click', function (e) {
   userChoice = prompt('How many squares across should the grid be?');
@@ -56,38 +32,106 @@ function calcGrid(squaresPerSide) {
   sketchContainer.classList = 'sketch-container';
   sketchContainerWrap.appendChild(sketchContainer);
 
+  const rainbowBtn = document.querySelector('.bg-rainbow')
 
-  
   for (let i = 0; i < gridArea; i++) {
     gridSquare = document.createElement('div');
     gridSquare.classList = 'grid-square';
-    
+
     gridSquare.style.cssText = `height:${gridHeight}%; flex: 0 0 ${gridWidth}%`;
-
-    const red = Math.floor(Math.random() * 256);
-    const green = Math.floor(Math.random() * 256);
-    const blue = Math.floor(Math.random() * 256);
-
-    const color = 'yellow';
+    // https://medium.com/codex/separating-touch-and-mouse-events-in-your-javascript-games-62181ae949f2
     
     sketchContainer.appendChild(gridSquare);
-    // console.log('Height: ', gridHeight)
-    // console.log('Width: ', gridWidth)
 
-    gridSquare.addEventListener('mouseover', function (e) {
-      // Each pass changes to a random RGB color. 
-      // The each pass after that adds 10% black. 
-      e.target.style.background = `rgb(${red}, ${green}, ${blue})`;
-      console.log(red, green, blue)
-    }); 
-
-    // rainbowBtn.addEventListener('click', function (e) {
-    //   // I need reference to all the gridSquares so I can change their color. 
-    //   gridSquare.toggleAttribute('red')
-    //   console.log(gridSquare)
-    // });
+    // gridSquare.addEventListener('mouseenter', function (e) { // mouseenter, mouseover
+    //   e.target.style.background = `rgb(${red}, ${green}, ${blue})`;
+    //   // console.log(this)
+    //   // Each pass changes to a random RGB color.
+    //   // The each pass after that adds 10% black. 
+    //   // console.log(red, green, blue)
+    //   // console.log('mouseenter')
+    //   // console.log(typeof e.target, 'array?', Array.isArray(e.target), e.target) // it's an object but not array
+    // }); 
+    
+    gridSquare.addEventListener('mouseover', calcBgColor); // mousedown
+    function calcBgColor(e) {
+      e.target.style.background = 'rgb(' + calcRandNum(0, 256) + ', ' + calcRandNum(0, 256) + ', ' + calcRandNum(0, 256) + ')';
+      // e.target.style.background = `rgb(randoNum(0, 256), randoNum(0, 256), randoNum(0, 256));`;
+      
+      // e.target.style.background = `rgb(${red}, ${green}, ${blue})`;
+    }
+    // gridSquare.addEventListener('mouseleave', function (e) { // mouseleave, mouseout
+    //   e.target.style.background = `rgb(${red}, ${green}, ${blue})`;
+    //   console.log('mouseleave')
+    //   console.log(red, green, blue)
+    // })
+    // gridSquare.addEventListener('mouseenter', gridBgChange);
   }
+  // gridSquare.removeEventListener('mouseover', handleBgChange);
+
   // console.log('squaresPerSide', squaresPerSide)
   // console.log('gridArea: ', gridArea)
 }
-calcGrid()
+calcGrid();
+
+function calcRandNum(min, max) {
+  const num = Math.floor(Math.random() * (max - min)) + min;
+  return num;
+};
+
+/*
+  https://stackoverflow.com/questions/3117756/javascript-create-element-and-set-attributes
+  const attrs = {
+    'id':     'idName',
+    'class':  'className'
+  }
+  function createElement(el, attrs) {
+    const element = document.createElement(el);
+    for (const val in attrs) {
+      //for support in the setAttrubute() method:
+      if (element.setAttribute) {
+        if (element[val] in element) {
+          element.setAttribute(val, attrs[val]);
+        } else {
+          element[val] = attrs[val];
+          // console.log(attrs[val], element[val])
+          }
+      } else {
+          element[val] = attrs[val];
+      }
+    }
+    return element;
+  }
+  console.log(createElement('span'))
+  const cSpan = createElement('p', {'id':'poop'});
+  cSpan.textContent = 'Hello, World!';
+  h1MainTitle.insertAdjacentElement('afterend', cSpan)
+*/
+
+// https://stackoverflow.com/questions/17264182/javascript-efficiently-insert-multiple-html-elements
+var msgContainer = document.createDocumentFragment();
+for (var i = 0; i < 3; i++) {
+    var option = msgContainer.appendChild(document.createElement("option"));
+    option.text = 'whatevs' //response.data[i].name;
+    option.value = 'whatevs' // response.data[i].id;
+}
+document.getElementById("etch-a-sketch").appendChild(msgContainer);
+// create is custom function
+// const msgContainer = document.createDocumentFragment();
+// for (var i = 0; i < 3; i++) {
+//     msgContainer.appendChild(create("option", {
+//         text:  'whatevs', // response.data[i].name,
+//         value:  'whatevs' // response.data[i].id
+//     }));
+// }
+// document.getElementById("friends").appendChild(msgContainer);
+
+function getColor() {
+  var letters = '0123456789ABCDEF';
+  var color = '#';
+  for (var i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)];
+  }
+  return color;
+}
+console.log(getColor())
