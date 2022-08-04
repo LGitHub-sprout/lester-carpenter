d375d784657617a58d5c6162839ddd83d4a2f543
LesterC <lsysworks@gmail.com>
2022-08-04 17:30:25 -0400

    EaS: Add 2nd calcRandNum(); Reorder HTML
    - js/etch-a-sketch.js
      * Add 2nd calcRandNum for 10% black mouseover event.
        - I know this is overkill but wanted to practice looping.
    
    - odin-project/etch-a-sketch.html
      * Reorder last and 'Just Try Something' section.
        - Put in reverse chronological order (like a blog).

--- js/etch-a-sketch.js
+++ js/etch-a-sketch.js
@@ -47,40 +47,45 @@ function calcGrid(squaresPerSide) {
     
     sketchContainer.appendChild(gridSquare);
 
-    // Each pass changes to a random RGB color.
-    // The each pass after that adds 10% black. 
-    // CSS variables would make changing the CSS properties easier @JensenIsHere TOP
-
-    let red   = Math.floor(Math.random() * 255);
-    let green = Math.floor(Math.random() * 255);
-    let blue  = Math.floor(Math.random() * 255);
-
     function changeBg() {
-
-      gridSquare.addEventListener('mouseover', calcBgColor, {
-        once: true
-      });
+      gridSquare.addEventListener('mouseover', calcBgColor);
       function calcBgColor(e) {
         // e.target.style.backgroundColor = 'rgb(' + red + ', ' + green + ', ' + blue + ')';
         // e.target.style.backgroundColor = 'green';
         // e.target.style.backgroundColor = 'rgba(' + calcRandNum(0, 255) + ', ' + calcRandNum(0, 255) + ', ' + calcRandNum(0, 255) + ')';
-        e.target.classList.add('black'); 
-        // Can I prevent continued events on first one AND add more to the same elements?
         // e.target.style.backgroundColor = 'purple';
         
+        // console.log(color[0]) // not defined
+        // console.log(red) // works bc it's already initialized above!!
+        // console.log('red', colorVals[0], 'green', colorVals[1], 'blue', colorVals[2])
+        // console.log(e.target.style.cssText)
         calcRandNum2(0, 255);
-        e.target.style.backgroundColor = 'rgb(' + red + ', ' + green + ', ' + blue + ')'; 
+        e.target.style.backgroundColor = 'rgb(' + colorVals[0] + ', ' + colorVals[1] + ', ' + colorVals[2] + ')'; 
+        // console.log('colorVals red', colorVals[0], 'colorVals green', colorVals[1], 'colorVals blue', colorVals[2])
       }
       // gridSquare.addEventListener('mouseover', whatever);
       // console.log(red, green, blue)
     }
-    const tenPercentRed = red / 10;
-    const tenPercentGreen = green / 10;
-    const tenPercentBlue = blue / 10;
+    changeBg();
+
+    // let red   = Math.floor(Math.random() * 255);
+    // let green = Math.floor(Math.random() * 255);
+    // let blue = Math.floor(Math.random() * 255);
+    calcRandNum2(0, 255);
+    // console.log('colorVals red', colorVals[0], 'colorVals green', colorVals[1], 'colorVals blue', colorVals[2])
+    let red = colorVals[0];
+    let green = colorVals[1];
+    let blue = colorVals[2];
+    // console.log('colorVals red', red, 'colorVals green', green, 'colorVals blue', blue)
+    // console.log('colorVals red', colorVals[0] / 10)
+    let tenPercentRed = red / 10;
+    let tenPercentGreen = green / 10;
+    let tenPercentBlue = blue / 10;
+    // // console.log('tenPercentRed', tenPercentRed)
     gridSquare.addEventListener('mouseover', function (e) {
+      // Wasn't working bc perhaps colorVals[#] can be used in ${var} notation?
       e.target.style.backgroundColor = `rgb(${red -= tenPercentRed}, ${green -= tenPercentGreen}, ${blue -= tenPercentBlue})`;
     })
-    changeBg();
     
     // Overrides previous event.
     // gridSquare.addEventListener('mouseover', function (e) {
@@ -104,32 +109,32 @@ function calcGrid(squaresPerSide) {
 }
 calcGrid();
 
-let red = Math.floor(Math.random() * 255);
-let green = Math.floor(Math.random() * 255);
-let blue  = Math.floor(Math.random() * 255);
+// let red = Math.floor(Math.random() * 255);
+// let green = Math.floor(Math.random() * 255);
+// let blue  = Math.floor(Math.random() * 255);
 
-console.log('red', red);
-console.log('green', green);
-console.log('blue', blue)
-const tenPercentRed = red / 10;
-const tenPercentGreen = green / 10;
-const tenPercentBlue = blue / 10;
-console.log('10% red', tenPercentRed)
-console.log('10% green', tenPercentGreen)
-console.log('10% blue', tenPercentBlue)
+// console.log('red', red);
+// console.log('green', green);
+// console.log('blue', blue)
+// const tenPercentRed = red / 10;
+// const tenPercentGreen = green / 10;
+// const tenPercentBlue = blue / 10;
+// console.log('10% red', tenPercentRed)
+// console.log('10% green', tenPercentGreen)
+// console.log('10% blue', tenPercentBlue)
 // red = red - tenPercentRed;
 // red -= tenPercentRed;
-const rainbowBtn2 = document.querySelector('.bg-rainbow');
-rainbowBtn2.addEventListener('click', (e) => {
-  // e.target.style.backgroundColor = `${red} -= ${tenPercentRed}`;
-  e.target.style.backgroundColor = `rgb(${red -= tenPercentRed}, ${green -= tenPercentGreen}, ${blue -= tenPercentBlue})`;
+// const rainbowBtn2 = document.querySelector('.bg-rainbow');
+// rainbowBtn2.addEventListener('click', (e) => {
+//   // e.target.style.backgroundColor = `${red} -= ${tenPercentRed}`;
+//   e.target.style.backgroundColor = `rgb(${red -= tenPercentRed}, ${green -= tenPercentGreen}, ${blue -= tenPercentBlue})`;
   // console.log('new red', red)
   // console.log('new green', green);
   // console.log('new blue', blue)
-  console.log('10% red', tenPercentRed)
-  console.log('10% green', tenPercentGreen)
-  console.log('10% blue', tenPercentBlue)
-})
+//   console.log('10% red', tenPercentRed)
+//   console.log('10% green', tenPercentGreen)
+//   console.log('10% blue', tenPercentBlue)
+// })
 
 // Forgot to credit the source of calcRandNum()  
 function calcRandNum(min, max) {
@@ -138,10 +143,12 @@ function calcRandNum(min, max) {
 };
 // const colors = ['red', 'green', 'blue'];
 function calcRandNum2(min, max) {
+  const randNum = Math.ceil(Math.random() * 25);
   colorVals = [];
   const colors = ['red', 'green', 'blue'];
   colors.forEach((color) => {
-    color = Math.floor(Math.random() * (max - min)) + min;
+    color = Math.floor(Math.random() * (max - min) + randNum);
+    if (color > 255) color -= (randNum * 2);
     colorVals.push(color);
   })
 }
@@ -175,12 +182,3 @@ function getColor() {
 // for (let i = 0; i < 5; i++) {
 //   createDomElement('p', 'poopClass', 'poopID', h1MainTitle)
 // }
-
-// rgb(0, 112, 184)
-// write a function that calcs the new RGB values
-// const red = 0 - (0 / 10);
-// const green = 112 - (112 / 10); 
-// const blue = 184 - (184 / 10);
-// console.log(red, green, blue)
-// h1MainTitle.style.backgroundColor = `rgb(${red}, ${green}, ${blue})`;
-
