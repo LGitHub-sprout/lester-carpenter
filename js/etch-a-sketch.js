427afd1c9070750d85f74d1938e258ee0778036e
LesterC <lsysworks@gmail.com>
2022-08-12 14:14:53 -0400

    EaS Clean Code; Add Last HTML Section
    - js/etch-a-sketch.js
      * Add document.Fragment, replaces create sketchContainer div.
      * Add pixels for height and width, replaces percentages.
    
    - odin-project/etch-a-sketch.html
      * Delete bg-rainbow button.
    
    - styles/odin-project-styles/odin.css
      * Clean code.
      * Change grid-square border.
      * Adjust .sketch-container-wrapper flex properties.

--- js/etch-a-sketch.js
+++ js/etch-a-sketch.js
@@ -1,10 +1,10 @@
 
 const sketchContainerWrap = document.querySelector('.sketch-container-wrapper');
 const setGridSizeBtn = document.querySelector('.set-grid-size');  
-const h1MainTitle = document.querySelector('.main-title');
 let userChoice;
 
 setGridSizeBtn.addEventListener('click', function (e) {
+
   userChoice = prompt('How many squares across should the grid be?');
   if ((userChoice > 100) || (userChoice === '') || Math.sign(userChoice) === -1) {
     alert('The maximum number of allowed squares is 100 and the minimum is 1.');
@@ -23,162 +23,71 @@ setGridSizeBtn.addEventListener('click', function (e) {
 function calcGrid(squaresPerSide) {
   if (userChoice) {
     squaresPerSide = userChoice;
-    sketchContainerWrap.firstElementChild.remove();
+    sketchContainerWrap.replaceChildren();
   } else squaresPerSide = 16;
 
   const gridArea = Math.pow(squaresPerSide, 2);
-  const gridHeight = Number(100 / squaresPerSide).toFixed(5);
-  const gridWidth = Number(100 / squaresPerSide).toFixed(5);
+  const gridHeight = Number(500 / squaresPerSide).toFixed(5);
+  const gridWidth = Number(500 / squaresPerSide).toFixed(5);
+
+  // const gridHeight = Number(100 / squaresPerSide).toFixed(5);
+  // const gridWidth = Number(100 / squaresPerSide).toFixed(5);
   
-  const sketchContainer = document.createElement('div');
-  sketchContainer.classList = 'sketch-container';
-  sketchContainerWrap.appendChild(sketchContainer);
-
-  const rainbowBtn = document.querySelector('.bg-rainbow')
-
-  // let gridSquare;
+  // https://dev.to/devcer/document-fragment-the-lesser-known-superpower-of-javascript-38ch
+  const sketchContainerFragment = new DocumentFragment();
+ 
+  // const sketchContainer = document.createElement('div');
+  sketchContainerFragment.classList = 'sketch-container';
+  sketchContainerWrap.appendChild(sketchContainerFragment);
 
   for (let i = 0; i < gridArea; i++) {
-    gridSquare = document.createElement('div');
+
+    const gridSquare = document.createElement('div');
     gridSquare.classList = 'grid-square';
+    gridSquare.style.height = `${gridHeight}px`;
+    gridSquare.style.width = `${gridWidth}px`;
+    // gridSquare.style.cssText = `height:${gridHeight}%; flex: 0 0 ${gridWidth}%;`;
 
-    gridSquare.style.cssText = `height:${gridHeight}%; flex: 0 0 ${gridWidth}%`;
-    // https://medium.com/codex/separating-touch-and-mouse-events-in-your-javascript-games-62181ae949f2
-    
-    sketchContainer.appendChild(gridSquare);
+    sketchContainerFragment.appendChild(gridSquare);
 
-    function changeBg() {
-      gridSquare.addEventListener('mouseover', calcBgColor);
-      function calcBgColor(e) {
-        // e.target.style.backgroundColor = 'rgb(' + red + ', ' + green + ', ' + blue + ')';
-        // e.target.style.backgroundColor = 'green';
-        // e.target.style.backgroundColor = 'rgba(' + calcRandNum(0, 255) + ', ' + calcRandNum(0, 255) + ', ' + calcRandNum(0, 255) + ')';
-        // e.target.style.backgroundColor = 'purple';
-        
-        // console.log(color[0]) // not defined
-        // console.log(red) // works bc it's already initialized above!!
-        // console.log('red', colorVals[0], 'green', colorVals[1], 'blue', colorVals[2])
-        // console.log(e.target.style.cssText)
-        calcRandNum2(0, 255);
-        e.target.style.backgroundColor = 'rgb(' + colorVals[0] + ', ' + colorVals[1] + ', ' + colorVals[2] + ')'; 
-        // console.log('colorVals red', colorVals[0], 'colorVals green', colorVals[1], 'colorVals blue', colorVals[2])
-      }
-      // gridSquare.addEventListener('mouseover', whatever);
-      // console.log(red, green, blue)
-    }
-    changeBg();
-
-    // let red   = Math.floor(Math.random() * 255);
-    // let green = Math.floor(Math.random() * 255);
-    // let blue = Math.floor(Math.random() * 255);
     calcRandNum2(0, 255);
-    // console.log('colorVals red', colorVals[0], 'colorVals green', colorVals[1], 'colorVals blue', colorVals[2])
+
     let red = colorVals[0];
     let green = colorVals[1];
     let blue = colorVals[2];
-    // console.log('colorVals red', red, 'colorVals green', green, 'colorVals blue', blue)
-    // console.log('colorVals red', colorVals[0] / 10)
+
     let tenPercentRed = red / 10;
     let tenPercentGreen = green / 10;
     let tenPercentBlue = blue / 10;
-    // // console.log('tenPercentRed', tenPercentRed)
+
     gridSquare.addEventListener('mouseover', function (e) {
-      // Wasn't working bc perhaps colorVals[#] can be used in ${var} notation?
       e.target.style.backgroundColor = `rgb(${red -= tenPercentRed}, ${green -= tenPercentGreen}, ${blue -= tenPercentBlue})`;
-    })
-    
-    // Overrides previous event.
-    // gridSquare.addEventListener('mouseover', function (e) {
-    //   e.target.style.backgroundColor = 'purple';
-    // })
-  }
-  /*
-    Needs to change div style on MouseOver, not click.
-  // rainbowBtn.addEventListener('click', function (e) {
-  //   rainbow.forEach(div => {
-  //     div.style.backgroundColor = 'rgba(' + calcRandNum(0, 255) + ', ' + calcRandNum(0, 255) + ', ' + calcRandNum(0, 255) + ')';
-  //   })
-  // });
-  // const rainbow = sketchContainer.querySelector('.grid-square');
-  const rainbow = sketchContainer.querySelectorAll('.grid-square');
-  // console.log(rainbow)
-  // rainbow.forEach(div => {
-    // div.style.backgroundColor = 'pink';
-  // })
-  */
+    });
+  } // for loop
+  sketchContainerWrap.appendChild(sketchContainerFragment);
 }
 calcGrid();
-
-// let red = Math.floor(Math.random() * 255);
-// let green = Math.floor(Math.random() * 255);
-// let blue  = Math.floor(Math.random() * 255);
-
-// console.log('red', red);
-// console.log('green', green);
-// console.log('blue', blue)
-// const tenPercentRed = red / 10;
-// const tenPercentGreen = green / 10;
-// const tenPercentBlue = blue / 10;
-// console.log('10% red', tenPercentRed)
-// console.log('10% green', tenPercentGreen)
-// console.log('10% blue', tenPercentBlue)
-// red = red - tenPercentRed;
-// red -= tenPercentRed;
-// const rainbowBtn2 = document.querySelector('.bg-rainbow');
-// rainbowBtn2.addEventListener('click', (e) => {
-//   // e.target.style.backgroundColor = `${red} -= ${tenPercentRed}`;
-//   e.target.style.backgroundColor = `rgb(${red -= tenPercentRed}, ${green -= tenPercentGreen}, ${blue -= tenPercentBlue})`;
-  // console.log('new red', red)
-  // console.log('new green', green);
-  // console.log('new blue', blue)
-//   console.log('10% red', tenPercentRed)
-//   console.log('10% green', tenPercentGreen)
-//   console.log('10% blue', tenPercentBlue)
+// Can I do the mouseover event outside the calcGrid() function?
+// const sketchContainer = sketchContainerWrap.lastElementChild;
+// const sketchContainer = sketchContainerWrap.firstElementChild;
+// console.log(sketchContainer)
+// const gridSquares = sketchContainer.querySelectorAll('.grid-square')
+// console.log('gridSquares', gridSquares) // NodeList
+// // Loop through NodeList and add RGB background color.
+// gridSquares.forEach(gridSquare => {
+//   gridSquare.addEventListener('mouseover', (e) => {
+//     e.target.style.backgroundColor = 'red';
+//   })
 // })
 
-// Forgot to credit the source of calcRandNum()  
-function calcRandNum(min, max) {
-  const num = Math.floor(Math.random() * (max - min)) + min;
-  return num;
-};
 // const colors = ['red', 'green', 'blue'];
 function calcRandNum2(min, max) {
-  const randNum = Math.ceil(Math.random() * 25);
+  // const randNum = Math.ceil(Math.random() * 25);
   colorVals = [];
   const colors = ['red', 'green', 'blue'];
   colors.forEach((color) => {
-    color = Math.floor(Math.random() * (max - min) + randNum);
-    if (color > 255) color -= (randNum * 2);
+    color = Math.floor(Math.random() * (max - min)); //  + randNum
+    // if (color > 255) color -= (randNum * 2);
     colorVals.push(color);
-  })
+  });
 }
-
-for (let i = 0; i < 3; i++) {
-  let color = Math.floor(Math.random() * 255);
-  // console.log('color', color)
-  // console.log(i);
-}
-
-// Return random hex color
-function getColor() {
-  var letters = '0123456789ABCDEF';
-  var color = '#';
-  for (var i = 0; i < 6; i++) {
-    color += letters[Math.floor(Math.random() * 16)];
-  }
-  return color;
-}
-// console.log(getColor())
-
-// const createDomElement = (element, className, idName, appendTo) => {
-//   el = document.createElement(element);
-//   el.classList.add(className);
-//   el.id = idName;
-//   el.textContent = 'Hello, World!';
-//   appendTo.insertAdjacentElement('afterend', el);
-
-//   return el
-// }
-// for (let i = 0; i < 5; i++) {
-//   createDomElement('p', 'poopClass', 'poopID', h1MainTitle)
-// }
